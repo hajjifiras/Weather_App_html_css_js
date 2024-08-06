98aa6d8a87addbb2412ed9fb1f9ae3d44ae0eed4
hajjifiras <firashajji59@gmail.com>
2024-08-06 17:25:23 +0100

    Errors Fixed

--- Test.js
+++ Test.js
@@ -1,31 +1,41 @@
+fetch(
+  "https://api.geoapify.com/v1/ipinfo?apiKey=a773af5971da4c61be0b1a4bf88db3a4"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    // You can now access the location data in the "data" object
+    document.getElementById("result").value = data.city.names.en.slice(
+      0,
+      data.city.names.en.indexOf(" ")
+    );
+    test();
+  });
 
 function test() {
   const city = document.getElementById("result").value;
   const api_key = "7abe26c6a3fa21cbd0ccc61d21cfeb03";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`;
 
-
   fetch(url)
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       const temp = data.main.temp;
       document.getElementById("result1").innerText = Math.round(temp) + "°C";
 
       const country = data.sys.country;
-      document.getElementById("place").innerText = city + ',' + country;
+      document.getElementById("place").innerText = city + "," + country;
 
       const deg = data.wind.deg;
       const wind = data.wind.speed;
-      document.getElementById("w").innerText = 'Wind speed: ' + Math.round(wind) + ' Km/h, Degree: ' + deg;
+      document.getElementById("w").innerText =
+        "Wind speed: " + Math.round(wind) + " Km/h, Degree: " + deg;
 
       const humidity = data.main.humidity;
-      document.getElementById("h").innerText = 'Humidity: ' + humidity + '%';
+      document.getElementById("h").innerText = "Humidity: " + humidity + "%";
 
       const description = data.weather[0].description;
       const main = data.weather[0].main;
-      document.getElementById("dis").innerText = main + ', ' + description;
-
-
+      document.getElementById("dis").innerText = main + ", " + description;
 
       const iconCode = data.weather[0].icon;
       const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
@@ -45,14 +55,6 @@ function test() {
         // Append the new icon element to the result2 element
         result2Element.appendChild(newIconElement);
       }
-
-
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
-
-
-
-
-
-
